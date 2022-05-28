ec79fda72a6e7d272594521158ac28d6af2a24c2
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-05-28 14:12:06 -0700

    Update Program.cs

--- ICommandAttribute_NullableParameter_Repro/Program.cs
+++ ICommandAttribute_NullableParameter_Repro/Program.cs
@@ -5,14 +5,35 @@ using ICommandAttribute_NullableParameter_Repro;
 var stringList = new List<string> { "Hello", "World" };
 
 var viewModel = new ViewModel();
+
+// No warning/error
 viewModel.M1Command.Execute((DateTime.UtcNow, "Hello World", true, stringList)); // No null values
+
+// No warning/error
 viewModel.M1Command.Execute((null, "Hello World", true, stringList)); // Pass `null` into Nullable struct
+
+// Yes Nullable Warning/Error
 viewModel.M1Command.Execute((DateTime.UtcNow, null, true, stringList)); // Pass `null` into Nullable string
+
+// No warning/error
 viewModel.M1Command.Execute((DateTime.UtcNow, "Hello World", null, stringList));  // Pass `null` into Nullable ValueType
+
+// Yes Nullable Warning/Error
 viewModel.M1Command.Execute((DateTime.UtcNow, "Hello World", true, null)); // Pass `null` into Nullable object
 
+
+// No warning/error
 await viewModel.M2Command.ExecuteAsync((DateTime.UtcNow, "Hello World", true, stringList)); // No null values
+
+// No warning/error
 await viewModel.M2Command.ExecuteAsync((null, "Hello World", true, stringList)); // Pass `null` into Nullable struct
+
+// Yes Nullable Warning/Error
 await viewModel.M2Command.ExecuteAsync((DateTime.UtcNow, null, true, stringList)); // Pass `null` into Nullable string
+
+// No warning/error
 await viewModel.M2Command.ExecuteAsync((DateTime.UtcNow, "Hello World", null, stringList)); // Pass `null` into Nullable ValueType
+
+// Yes Nullable Warning/Error
 await viewModel.M2Command.ExecuteAsync((DateTime.UtcNow, "Hello World", true, null)); // Pass `null` into Nullable object
+    }
